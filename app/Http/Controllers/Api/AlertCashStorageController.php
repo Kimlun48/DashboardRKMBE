a76adc81c025ed7c75e2a14b6d16508b5bf4bf64
kimlun48 <asepsuherman1984@gmail.com>
2024-06-27 16:27:10 +0700

    cashstorage

--- app/Http/Controllers/Api/AlertCashStorageController.php
+++ app/Http/Controllers/Api/AlertCashStorageController.php
@@ -43,7 +43,7 @@ class AlertCashStorageController extends Controller
 
         return response()->json([
             'success' => true,
-            'message' => 'Statistik Data',
+            'message' => 'Statistik Data Alert Cash Storage',
             'data' => [
                 'late' => $late,
                 'unlate' => $unlate,
